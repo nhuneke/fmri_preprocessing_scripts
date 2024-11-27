62ea2b8e370116a4be6d7204f912b8c1a4660eb5
Nathan <n.huneke@soton.ac.uk>
2024-11-27 13:20:24 +0000

    Remove backup steps from preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -15,6 +15,7 @@ echo "Preprocessing ${subj}"
 docker run -it --rm -v /local/scratch/nh6g15/datasets/roar_preproc/raw:/data:ro -v /local/scratch/nh6g15/datasets/roar_bids_qc/derivatives:/out nipreps/mriqc:latest /data /out participant --participant-label $label
 
 # Do fmriprep preprocessing
+##### Change paths #####
 fmriprep-docker ./raw  ./${sub} \
 	participant --participant-label ${label} \
 	--fs-no-reconall \
@@ -22,8 +23,4 @@ fmriprep-docker ./raw  ./${sub} \
 	--skip-bids-validation \
 	--fs-license-file /local/software/freesurfer/license.txt
 
-# Back up outputs
-datalad push -d ${subj} --to ria-backup  # Back up subject subdataset
-datalad push --to ria-backup  # Back up superdataset
-
-echo "Preprocessing for ${subj} complete and backed up to research filestore. Please update gitlab sibling."
+echo "Preprocessing for ${subj} complete."
