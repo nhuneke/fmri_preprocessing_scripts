05cebc9308b0ed14df8c041bbb493f50b2dbb77f
Nathan <n.huneke@soton.ac.uk>
2024-12-06 13:56:38 +0000

    Update conversion scripts for BADHD data

--- batch_convert.sh
+++ batch_convert.sh
@@ -6,8 +6,7 @@ set -u
 subs=("32" "34" "35" "36" "37" "38" "39")
 
 for id in "${subs[@]}" ; do
-	subj="sub-$id"
-	code/preprocess.sh ${subj}
+	code/convert_to_bids.py $id
 done
 
-echo "Batch pre-processing completed successfully"
+echo "Conversion to nifti completed successfully"
