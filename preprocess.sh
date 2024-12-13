ffe7ee82ef209ff73ad74c9c29f27d0c3c62964d
Nathan <n.huneke@soton.ac.uk>
2024-12-13 15:12:53 +0000

    Update preprocess.sh for BADHD

--- preprocess.sh
+++ preprocess.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# Command is "bash code/preprocess.sh sub-ID".
+# From adhd-rest/ command is "bash bids/code/fmri_preprocessing_scripts/preprocess.sh".
 
 set -e -u
 
-subj="$1"
-prefix="sub-"
-label=${subj/#$prefix}
+subs=("32" "34" "35" "36" "37" "38" "39")
 
-echo "Preprocessing ${subj}"
+for label in "${subs[@]}" ; do
 
-# Run MRIQC
-##### Change paths #####
-docker run -it --rm -v /local/scratch/nh6g15/datasets/roar_preproc/raw:/data:ro -v /local/scratch/nh6g15/datasets/roar_bids_qc/derivatives:/out nipreps/mriqc:latest /data /out participant --participant-label $label
+	echo "Preprocessing ${label}"
 
-# Do fmriprep preprocessing
-##### Change paths #####
-fmriprep-docker ./raw  ./${sub} \
-	participant --participant-label ${label} \
-	--fs-no-reconall \
-	--user $( id -u ) \
-	--skip-bids-validation \
-	--fs-license-file /local/software/freesurfer/license.txt
+	# Run MRIQC
+	##### Change paths #####
+	docker run -it --rm -v /local/scratch/cs1n24/adhd-rest/bids:/data:ro \
+		-v /local/scratch/adhd-rest/derivatives:/out \
+		nipreps/mriqc:latest \
+		/data /out participant --participant-label $label
 
-echo "Preprocessing for ${subj} complete."
+	# Do fmriprep preprocessing
+	##### Change paths #####
+	fmriprep-docker ./bids  ./derivatives \
+		participant --participant-label $label \
+		--fs-no-reconall \
+		--user $( id -u ) \
+		--skip-bids-validation \
+		--fs-license-file /local/software/freesurfer/license.txt
+
+	echo "Preprocessing for ${label} complete."
+done
+
+echo "All subjects pre-processed"
